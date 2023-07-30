e38b0e59143c5c3ab87303f6bb3b087dbc818fc3
manylayers <domatthew1854@gmail.com>
2023-07-30 02:04:22 -0700

    updated and complete proj

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ const barplot_annotations =
                 wrap: 200
             },
             color: ["black"],
-            x: 240,
-            y: 300,
+            x: 350,
+            y: 400,
             dx: 50,
-            dy: 50
+            dy: -50
             }
         ]
 ];
@@ -76,11 +76,44 @@ const barplot_median_annotations =
         ]
 ];
 
+const barplot_median_income_annotations = 
+    [
+        [
+            {
+            note: {
+                label: 'There exists a correlation between household income and household median home',
+                title: 'Insight #1',
+                wrap: 200
+            },
+            color: ["black"],
+            x: 100,
+            y: 250,
+            dx: 50,
+            dy: -50
+            }
+        ],
+        [
+            {
+            note: {
+                label: 'Households near oceans will have on median a higher income than their counterparts more inland. Except Islands of course.',
+                title: 'Insight #2',
+                wrap: 200
+            },
+            color: ["black"],
+            x: 600,
+            y: 200,
+            dx: -50,
+            dy: 50
+            }
+        ]
+];
+
 async function init() {
     console.log("data loading")
     // data = await d3.csv('https://raw.githubusercontent.com/imsatyamshandilya/ML-housing_price_prediction/master/housing.csv');
     barplot_data = await d3.csv('https://raw.githubusercontent.com/domatt1854/housing-data/main/ocean_proximity_counts.csv');
     barplot_data_median = await d3.csv('https://raw.githubusercontent.com/domatt1854/housing-data/main/ocean_proximity_median_value.csv')
+    barplot_data_income_median = await d3.csv('https://raw.githubusercontent.com/domatt1854/housing-data/main/ocean_proximity_median_income.csv')
     slide_number = 1;
     await renderBarplot();
     console.log("done");
@@ -111,9 +144,11 @@ async function renderVisualization(state) {
         await renderBarplot(0);
     }    
     else if ( slide_number == 2 ) {
-        await renderBarPlot2(0);
+        await renderBarPlot3(0);
     }
-
+    else if( slide_number == 3) {
+        renderBarPlot2(slide_annotation_index);
+    }
 }
 
 async function updateAnnotationIndex() { 
@@ -123,6 +158,9 @@ async function updateAnnotationIndex() {
         renderBarplot(slide_annotation_index);
     }
     else if (slide_number == 2) {
+        renderBarPlot3(slide_annotation_index);
+    }
+    else if( slide_number == 3) {
         renderBarPlot2(slide_annotation_index);
     }
 }
@@ -178,6 +216,56 @@ async function renderBarplot(slide_annotation_index) {
 }
 
 async function renderBarPlot2(slide_annotation_index) {
+    var x = d3.scaleBand()
+        .range([0, width])
+        .domain(barplot_data_income_median.map(function(d) { return d.ocean_proximity; }));
+    
+    var y = d3.scaleLinear()
+        .domain([0, 60000])
+        .range([height, 0]);
+    
+    svg.append("g")
+        .attr("transform", "translate(" + margin + ", " + (height - margin) + ")")
+        .call(d3.axisBottom(x));
+    
+    svg.append("g")
+        .attr("transform", "translate(" + margin + ", -" + margin + ")")
+        .call(d3.axisLeft(y));
+    
+    svg.selectAll("barplot").data(barplot_data_income_median)
+        .enter()
+        .append("rect")
+        .attr("transform", "translate(" + 88 + ", -" + margin + ")")
+        .attr("x", function(d) { return x(d.ocean_proximity); })
+        .attr("y", function(d) { return y(d.median_income); })
+        .attr("width", x.bandwidth() - 75)
+        .attr("height", function(d) { return height - y(d.median_income); })
+        .attr("fill", "#69b3a2");
+    
+    svg.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", (margin / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text("Household Median Income According to Their Location W.R.T. the Ocean");
+    
+    if( slide_annotation_index != 0 && slide_annotation_index < barplot_median_income_annotations.length + 1) {
+
+        console.log("slide annotation index: " + slide_annotation_index);
+        const annotation = d3.annotation()
+            .annotations(barplot_median_income_annotations[(slide_annotation_index - 1)]);
+        
+        console.log(barplot_median_income_annotations[(slide_annotation_index - 1)]);
+
+        console.log("annotation updated, accessing: " + (slide_annotation_index - 1));
+        svg.append("g")
+            .attr("class", "annotation-group")
+            .call(annotation);
+    }
+}
+
+async function renderBarPlot3(slide_annotation_index) {
     var x = d3.scaleBand()
         .range([0, width])
         .domain(barplot_data_median.map(function(d) { return d.ocean_proximity; }));
